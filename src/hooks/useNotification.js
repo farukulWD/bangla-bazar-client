c9f0263bdd7b98ba2b7442e62d2463edbf7de2f7
farukulWD <farukgb1999@gmail.com>
2025-04-25 20:51:03 +0600

    refactor: uncomment socket initialization and event listeners in useNotification hook

--- src/hooks/useNotification.js
+++ src/hooks/useNotification.js
@@ -1,7 +1,7 @@
 import { addSetting, removeSetting } from "@redux/slice/settingSlice";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
-// import io from "socket.io-client";
+import io from "socket.io-client";
 
 // Create a single socket instance
 // const socket = io(process.env.NEXT_PUBLIC_API_SOCKET_URL);
@@ -13,41 +13,41 @@ const useNotification = () => {
 
   // console.log("socket", socket);
 
-  // useEffect(() => {
-  //   // setSocket(io(process.env.NEXT_PUBLIC_API_SOCKET_URL));
-  //   setSocket(io("http://localhost:5055"));
-  // }, []);
+  useEffect(() => {
+    setSocket(io(process.env.NEXT_PUBLIC_API_SOCKET_URL));
+    // setSocket(io("http://localhost:5055"));
+  }, []);
 
-  // useEffect(() => {
-  //   // Listen for the 'notification' event from the server
-  //   socket?.on("notification", (notification) => {
-  //     // Update data in real-time here
-  //     console.log("notification", notification);
-  //     if (notification?.option === "globalSetting") {
-  //       dispatch(removeSetting("globalSetting"));
-  //       const globalSettingData = {
-  //         ...notification.globalSetting,
-  //         name: "globalSetting",
-  //       };
-  //       dispatch(addSetting(globalSettingData));
-  //     }
-  //     if (notification?.option === "storeCustomizationSetting") {
-  //       dispatch(removeSetting("storeCustomizationSetting"));
+  useEffect(() => {
+    // Listen for the 'notification' event from the server
+    socket?.on("notification", (notification) => {
+      // Update data in real-time here
+      console.log("notification", notification);
+      if (notification?.option === "globalSetting") {
+        dispatch(removeSetting("globalSetting"));
+        const globalSettingData = {
+          ...notification.globalSetting,
+          name: "globalSetting",
+        };
+        dispatch(addSetting(globalSettingData));
+      }
+      if (notification?.option === "storeCustomizationSetting") {
+        dispatch(removeSetting("storeCustomizationSetting"));
 
-  //       const storeCustomizationSettingData = {
-  //         ...notification.storeCustomizationSetting,
-  //         name: "storeCustomizationSetting",
-  //       };
-  //       dispatch(addSetting(storeCustomizationSettingData));
-  //     }
-  //   });
+        const storeCustomizationSettingData = {
+          ...notification.storeCustomizationSetting,
+          name: "storeCustomizationSetting",
+        };
+        dispatch(addSetting(storeCustomizationSettingData));
+      }
+    });
 
-  //   return () => {
-  //     // Disconnect the socket when the component unmounts
-  //     socket?.disconnect();
-  //   };
-  // }, [socket]);
-  // console.log("socket", socket);
+    return () => {
+      // Disconnect the socket when the component unmounts
+      socket?.disconnect();
+    };
+  }, [socket]);
+  console.log("socket", socket);
 
   return {
     socket, // You can still return the socket instance if needed
